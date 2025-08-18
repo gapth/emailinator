881347581ca318d3efdb946de170b5ebac8cf8db
Gap <gapthirathon@gmail.com>
2025-08-17 21:34:25 -0700

    Fix logging. Info log will show up in web log.

--- src/emailinator/__init__.py
+++ src/emailinator/__init__.py
@@ -1,1 +1,29 @@
+"""Emailinator package initialization and logging setup."""
+
+import logging
+import sys
+
 __version__ = "0.1.0"
+
+
+def _configure_logger() -> None:
+    """Configure a package level logger that logs to stdout.
+
+    Uvicorn configures its own loggers and does not automatically forward
+    library loggers such as ``emailinator``.  By attaching a ``StreamHandler``
+    to the package logger we ensure that calls to ``logging.getLogger("emailinator")``
+    produce output when running ``make run`` or ``make web``.
+    """
+
+    logger = logging.getLogger("emailinator")
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.propagate = False
+    logger.setLevel(logging.INFO)
+
+
+_configure_logger()
+
